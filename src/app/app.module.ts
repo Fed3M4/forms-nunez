c16938c48e70abce80319ac84d29cd9fd240bf11
Federico <nunez.fedema@gmmail.com>
2023-02-03 01:40:22 -0300

    correcciones de bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HeaderComponent } from "./componentes/header/header.component";
 import { FormularioComponent } from "./componentes/formulario/formulario.component";
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
-        FormularioComponent
+        FormularioComponent,
+        FooterComponent
     ],
     providers: [],
     bootstrap: [AppComponent],
